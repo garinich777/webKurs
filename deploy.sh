40da276740fc52328c9896976e02b309e3f3bb7c
garinich777 <777programmer777@gmail.com>
2023-10-11 04:16:07 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,8 @@ cd dist
 # если вы публикуете на пользовательский домен
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
-
 # если вы публикуете по адресу https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:garinich777/garinich777.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
